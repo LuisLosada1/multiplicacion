8bc4d7505c1dde3b66d0c2056a0becab5845c682
Caldas <caldas@laboratorios.local>
2022-09-17 08:58:22 -0500

    Nuevo archivo

---
+++ Hola.java
@@ -0,0 +1,10 @@
+
+
+public class Hola{
+
+  public static void main(String[] args){
+  System.out.println("Hola mundo");
+  Multi multiplicar = new Multi();
+  System.out.println(multiplicar.multiplicacion (10));
+ }
+}
---
+++ Multi.java
@@ -0,0 +1,13 @@
+
+public class Multi{
+
+ public int multiplicacion(int 2){
+
+  if (2==1)
+     return 0;
+  if (a==2)
+     return 1;
+  return multiplicacion (2*1)+multiplicacion(2*2);
+  
+ }
+}
